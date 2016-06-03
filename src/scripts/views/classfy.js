e7f29740be87b2fd6b37a0b289d34dc151bc88ba
1542814620 <1542814620@qq.com>
2016-06-03 21:14:30 +0800

    201606

--- src/scripts/views/classfy.js
+++ src/scripts/views/classfy.js
@@ -3,22 +3,26 @@ var classfyTpl = require('../tpl/classfy.string');
 SPA.defineView('classfy',{
 	html:classfyTpl,
 	plugins: ['delegated'],
+
+	bindActions: {
+	    'switch.swiper': function (e) {
+	      $(e.el).addClass('active').siblings().removeClass('active');
+	      this.classfySwiper.slideTo($(e.el).index());
+	      console.log($(e.el).index()+1);
+	    }
+	},
+
 	init:{
 		classfySwiper: null
 	},
-	bindActions: {
-	    'switch.swiper': function (e) {
-	      this.setActive($(e.el));
-	      this.classfySwiper.slideTo($(e.el).index()+1);
-	    }
-	},  
+
 	bindEvents:{
 		'beforeShow':function(){
 			 this.classfySwiper = new Swiper('#classfy-swiper',{
 				direction: 'vertical',
-				loop: true,
 		        onSlideChangeStart: function (swiper) {
-		          $('.classfy1 li').eq(swiper.activeIndex-1)
+		        	//console.log(swiper.activeIndex)
+		          $('.classfy1 li').eq(swiper.activeIndex)
 		            .addClass('active').siblings().removeClass('active');
 		        }
 			})
